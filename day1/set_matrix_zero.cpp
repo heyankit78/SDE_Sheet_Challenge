09a2abcd7e81a78866f6ec096ba3cf18b081ab51
Ankit Kumar Ghosh <76126678+heyankit78@users.noreply.github.com>
2022-06-04 22:28:29 +0530

    Update set_matrix_zero.cpp

--- day1/set_matrix_zero.cpp
+++ day1/set_matrix_zero.cpp
@@ -1,1 +1,42 @@
-nsf  
+ int n = matrix.size();
+      int m = matrix[0].size();
+      
+      int x = -1;
+      int y = -1;
+      for(int j=0;j<m;j++){
+        if(matrix[0][j] == 0)x=0;
+      }
+      for(int i=0;i<n;i++){
+        if(matrix[i][0] == 0)y=0;
+      }
+      for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+          if(matrix[i][j] == 0){
+            matrix[0][j] = 0;
+            matrix[i][0] = 0;
+          }
+        }
+      }
+      for(int j=1;j<m;j++){
+        if(matrix[0][j] == 0){
+          for(int i=1;i<n;i++){
+            matrix[i][j] = 0;
+          }
+        }
+      }
+      for(int i=1;i<n;i++){
+        if(matrix[i][0] == 0){
+          for(int j=1;j<m;j++){
+            matrix[i][j] = 0;
+          }
+        }
+      }
+      if(y == 0){
+        for(int i=0;i<n;i++)
+          matrix[i][0] = 0;
+      }
+      if(x == 0){
+        for(int j=0;j<m;j++)
+          matrix[0][j] = 0;
+      }
+    }
